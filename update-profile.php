c1c07697e2ac2d730b526453a2f4b04d55f17861
sn-sweet-stack <sergey@sweetstackdigital.com>
2023-12-05 10:57:32 +0000

    result

--- update-profile.php
+++ update-profile.php
@@ -22,48 +22,104 @@ session_start();
 // Log the user automatically for this demo - this is ok
 $_SESSION['user_id'] = 5;
 
-$db     = new Db;
-$result = $db->query('SELECT * FROM `users` WHERE `id` = ?', [$_SESSION['user_id']]);
-$user   = $result->fetch_assoc();
-
 $data = $_POST;
-$updates = "";
 
-if (!empty($data['password']) && (!isset($data['current_password']) || !password_verify($data['current_password'], $user['password']))) {
-    // do not let the user update password
-    die('{ "status": "error", "message": "Current password is incorrect" }');
-}
 
-// encrypt the password
-if (isset($data['password'])) {
-    $data['password'] = password_hash($data['password'], PASSWORD_BCRYPT);
-}
+class Profile {
 
-unset($data['current_password']);
-
-if (empty($data['name'])) {
-    die('{ "status": "error", "message": "Name cannot be empty" }');
-}
-
-if (empty($data['email'])) {
-    die('{ "status": "error", "message": "Email cannot be empty" }');
-}
-
-if (!preg_match('/^[\w\.-]+@[a-zA-Z\d\.-]+\.[a-zA-Z]{2,}$/', $data['email'])) {
-    die('{ "status": "error", "message": "Email is invalid" }');
-}
-
-$countFields = 0;
-foreach ($data as $field => $value) {
-    $updates .= "$field = '$value'";
-    $countFields++;
-    if ($countFields < count($data)) {
-      $updates .= ", ";
+  protected $userId;
+  protected $data;
+  protected $userModel;
+  protected $validateErrors = [];
+  
+  public function update($data) 
+  {
+    $this->data = $data;
+    
+    if (!$this->validate()) {
+      return false;
     }
+
+
+    if (!empty($this->data['password'])) {
+      $this->updatePassword();
+    }
+  
+    $this->updateUserData();
+  }
+
+  public function getErrors()
+  {
+    return $this->validateErrors;
+  }
+
+  public function __construct($userId)
+  {
+    $this->userId = $userId;
+    $this->userModel = $this->getUser();
+
+    if (empty($this->userModel)) {
+      $this->validateErrors[] = 'User not found';
+    }
+  }
+
+  protected function getUser()
+  {
+    $db     = new Db;
+    $result = $db->query('SELECT * FROM `users` WHERE `id` = ?', [$_SESSION['user_id']]);
+    $user   = $result->fetch_assoc();
+
+    return $user;
+  }
+
+  protected function updatePassword() 
+  {
+    if (isset($this->data['password'])) {
+      $this->data['password'] = password_hash($this->data['password'], PASSWORD_BCRYPT);
+    }
+
+    unset($this->data['current_password']);
+
+    $db->query("UPDATE users SET password = ? WHERE id = ?", $this->data['password'], $this->userId);
+  }
+
+  protected function updateUserData() 
+  {
+    foreach ($this->data as $field => $value) {
+      $db->query("UPDATE users SET ? = ? WHERE id = ?", $field, $value, $this->userId);
+    }
+  }
+
+  protected function validate()
+  {
+    if (!empty($data['password']) && (!isset($data['current_password']) || !password_verify($data['current_password'], $user['password']))) {
+        // do not let the user update password
+      $this->validateErrors[] = '{ "status": "error", "message": "Current password is incorrect" }';
+    }
+    
+    if (empty($data['name'])) {
+      $this->validateErrors[] = '{ "status": "error", "message": "Name cannot be empty" }';
+    }
+
+    if (empty($data['email'])) {
+      $this->validateErrors[] = '{ "status": "error", "message": "Email cannot be empty" }';
+    }
+
+    if (!preg_match('/^[\w\.-]+@[a-zA-Z\d\.-]+\.[a-zA-Z]{2,}$/', $data['email'])) {
+      $this->validateErrors[] = '{ "status": "error", "message": "Email is invalid" }';
+    }
+  }
+
+  return empty($this->validateErrors);
 }
 
-$sql = "UPDATE users SET $updates WHERE id = {$user['id']}";
 
-$db->query($sql);
+$profile = new Profile($_SESSION['user_id']);
 
-echo '{ "status": "ok", "message": "Profile is updated!" }';
+$success = $profile->update($data);
+
+if ($success) {
+  echo '{ "status": "ok", "message": "Profile is updated!" }';
+} else {
+  print_r($profile->getErrors());
+}
