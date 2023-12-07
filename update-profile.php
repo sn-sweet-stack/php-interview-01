03448d055c25c2cba997ca960d93b2cf4d0d3e49
sn-sweet-stack <sergey@sweetstackdigital.com>
2023-12-07 19:13:33 +0000

    result

--- update-profile.php
+++ update-profile.php
@@ -22,24 +22,11 @@ session_start();
 // Log the user automatically for this demo - this is ok
 $_SESSION['user_id'] = 5;
 
-$db     = new Db;
-$result = $db->query('SELECT * FROM `users` WHERE `id` = ?', [$_SESSION['user_id']]);
-$user   = $result->fetch_assoc();
-
-$data = $_POST;
-$updates = "";
-
-if (!empty($data['password']) && (!isset($data['current_password']) || !password_verify($data['current_password'], $user['password']))) {
-    // do not let the user update password
-    die('{ "status": "error", "message": "Current password is incorrect" }');
-}
-
-// encrypt the password
-if (isset($data['password'])) {
-    $data['password'] = password_hash($data['password'], PASSWORD_BCRYPT);
-}
-
-unset($data['current_password']);
+$data = [];
+$data['name'] = $_POST['name'];
+$data['email'] = $_POST['email'];
+$data['password'] = $_POST['password'];
+$data['current_password'] = $_POST['current_password'];
 
 if (empty($data['name'])) {
     die('{ "status": "error", "message": "Name cannot be empty" }');
@@ -53,17 +40,25 @@ if (!preg_match('/^[\w\.-]+@[a-zA-Z\d\.-]+\.[a-zA-Z]{2,}$/', $data['email'])) {
     die('{ "status": "error", "message": "Email is invalid" }');
 }
 
-$countFields = 0;
-foreach ($data as $field => $value) {
-    $updates .= "$field = '$value'";
-    $countFields++;
-    if ($countFields < count($data)) {
-      $updates .= ", ";
-    }
+
+$db     = new Db;
+$result = $db->query('SELECT * FROM `users` WHERE `id` = ?', [$_SESSION['user_id']]);
+$user   = $result->fetch_assoc();
+
+
+if (!empty($data['password']) && (!isset($data['current_password']) || !password_verify($data['current_password'], $user['password']))) {
+    // do not let the user update password
+    die('{ "status": "error", "message": "Current password is incorrect" }');
 }
 
-$sql = "UPDATE users SET $updates WHERE id = {$user['id']}";
+// encrypt the password
+if (isset($data['password'])) {
+    $data['password'] = password_hash($data['password'], PASSWORD_BCRYPT);
+}
 
-$db->query($sql);
+
+$sql = "UPDATE users SET `name` = ?,  `email` = ?, `password` = ? WHERE id = {$user['id']}";
+
+$db->query($sql, [$data[name], $data[email], $data[password]]);
 
 echo '{ "status": "ok", "message": "Profile is updated!" }';
